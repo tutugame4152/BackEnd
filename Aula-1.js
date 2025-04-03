22a5896425dd0f7b46235b8814a188fa850e9076
tutugame4152 <arthuraiamm@gmail.com>
2025-04-03 16:26:53 -0400

    primeira tarefa

---
+++ Aula-1.js
@@ -0,0 +1,7 @@
+let valor = 51.51515;
+let text = "Meu primeiro projeto em Java script";
+let booleano = true;
+
+console.log(valor.toFixed(2));//Arredonda pra 2 casas decimais
+console.log(text.toUpperCase());//Converte o texto para maiúsculas
+console.log(booleano.toString());//Converte para texto
