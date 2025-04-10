03d4a2187d4922112c3b440c49a43d4baf1d729e
tutugame4152 <arthuraiamm@gmail.com>
2025-04-10 17:21:03 -0400

    atualização

--- aula 2/Exercicio 4.js
+++ aula 2/Exercicio 4.js
@@ -1,4 +1,4 @@
 let peso = 32
 let altura = 2
 let imc = peso / (altura*altura)
-console.log("O IMC é: "+ imc)
+console.log("O IMC é: "+ imc.toFixed(2))
---
+++ aula 3/exercicios.js
@@ -0,0 +1,24 @@
+//1
+
+let varia1 = 10;
+let varia2 = 2;
+console.log("soma" + varia1 + varia2);
+console.log("Subtração" + varia1 - varia2);
+console.log("Multiplicação" + varia1 * varia2);
+console.log("Divisão" + varia1 / varia2);
+
+//2
+
+let idade1 = 20;
+let idade2 = 21;
+console.log(idade1 > idade2 ? "O primeiro é mais velho" : "O segundo é mais velho");
+
+//3
+
+let idadeaa = 26;
+console.log(idadeaa >= 18 ? "É maior de idade" : "É menor de idade");
+
+//4
+
+let numero = 5;
+console.log(numero % 2 == 0 ? "É par" : "É impar");
